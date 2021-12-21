45249109eff7fc3dd5c720a497a6455d12401a59
Liuian <liuperong@gmail.com>
2021-12-21 16:11:32 +0800

    make it better

--- fsm.py
+++ fsm.py
@@ -42,7 +42,7 @@ class TocMachine(GraphMachine):
         # send_image_message(event.reply_token, url)
         
         title = '紅色-穿搭顏色心理學'
-        text = '愛穿紅色的人，大多具有冒險精神，勇於接受挑戰、擁有開闊的心胸接納新鮮事物，通常也擅長處理人際關係。'
+        text = '愛穿紅色的人，具有冒險精神、勇於接受挑戰、擁有開闊的心胸接納新鮮事物，也擅長處理人際關係。'
         btn = [
             MessageTemplateAction(
                 label = '特定場合穿搭顏色建議',
@@ -67,10 +67,25 @@ class TocMachine(GraphMachine):
         return text.lower() == "2"
 
     def on_enter_blue(self, event):
-        print("I'm entering state1")
-
+        '''print("I'm entering state1")
         reply_token = event.reply_token
         send_text_message(reply_token, "blue\n（重新開始）\n'1' 特定場合顏色穿搭'2' 穿搭顏色心理學")
+        self.go_back()'''
+        title = '藍色-穿搭顏色心理學'
+        text = '喜歡穿藍色的人往往理智且冷靜，容易接納並包容周邊的人事物。'
+        btn = [
+            MessageTemplateAction(
+                label = '特定場合穿搭顏色建議',
+                text ='1'
+            ),
+            MessageTemplateAction(
+                label = '穿搭顏色心理學',
+                text = '2'
+            ),
+        ]
+        url = 'https://i.imgur.com/aJMwFQp.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
         self.go_back()
 
     def on_exit_blue(self):
@@ -81,10 +96,25 @@ class TocMachine(GraphMachine):
         return text.lower() == "3"
 
     def on_enter_green(self, event):
-        print("I'm entering state1")
-
+        '''print("I'm entering state1")
         reply_token = event.reply_token
         send_text_message(reply_token, "green\n（重新開始）\n'1' 特定場合顏色穿搭'2' 穿搭顏色心理學")
+        self.go_back()'''
+        title = '綠色-穿搭顏色心理學'
+        text = '喜歡綠色的人通常處於穩定的生活環境裡，會主動關心並照顧朋友，態度積極、社交能力強。'
+        btn = [
+            MessageTemplateAction(
+                label = '特定場合穿搭顏色建議',
+                text ='1'
+            ),
+            MessageTemplateAction(
+                label = '穿搭顏色心理學',
+                text = '2'
+            ),
+        ]
+        url = 'https://i.imgur.com/slTmgrM.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
         self.go_back()
 
     def on_exit_green(self):
@@ -95,10 +125,25 @@ class TocMachine(GraphMachine):
         return text.lower() == "4"
 
     def on_enter_yellow(self, event):
-        print("I'm entering state1")
-
+        '''print("I'm entering state1")
         reply_token = event.reply_token
         send_text_message(reply_token, "yellow\n（重新開始）\n'1' 特定場合顏色穿搭'2' 穿搭顏色心理學")
+        self.go_back()'''
+        title = '黃色-穿搭顏色心理學'
+        text = '鮮豔的黃色給人活潑開朗的印象，就像小小兵一般擁有天真爛漫的內心，對生活富有熱情、擁有非凡的創意力。'
+        btn = [
+            MessageTemplateAction(
+                label = '特定場合穿搭顏色建議',
+                text ='1'
+            ),
+            MessageTemplateAction(
+                label = '穿搭顏色心理學',
+                text = '2'
+            ),
+        ]
+        url = 'https://i.imgur.com/wyeqLQ1.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
         self.go_back()
 
     def on_exit_yellow(self):
@@ -109,10 +154,25 @@ class TocMachine(GraphMachine):
         return text.lower() == "5"
 
     def on_enter_purple(self, event):
-        print("I'm entering state1")
-
+        '''print("I'm entering state1")
         reply_token = event.reply_token
         send_text_message(reply_token, "purple\n（重新開始）\n'1' 特定場合顏色穿搭'2' 穿搭顏色心理學")
+        self.go_back()'''
+        title = '紫色-穿搭顏色心理學'
+        text = '喜歡紫色的人通常喜歡幻想、情感豐沛且浪漫，對生活充滿激情，在藝術方面很有天分。'
+        btn = [
+            MessageTemplateAction(
+                label = '特定場合穿搭顏色建議',
+                text ='1'
+            ),
+            MessageTemplateAction(
+                label = '穿搭顏色心理學',
+                text = '2'
+            ),
+        ]
+        url = 'https://i.imgur.com/KxKObmx.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
         self.go_back()
 
     def on_exit_purple(self):
@@ -123,10 +183,25 @@ class TocMachine(GraphMachine):
         return text.lower() == "6"
 
     def on_enter_wite(self, event):
-        print("I'm entering state1")
-
+        '''print("I'm entering state1")
         reply_token = event.reply_token
         send_text_message(reply_token, "wite\n（重新開始）\n'1' 特定場合顏色穿搭'2' 穿搭顏色心理學")
+        self.go_back()'''
+        title = '白色-穿搭顏色心理學'
+        text = '喜歡穿白色衣服的人是典型的完美主義者，個性樂觀、追求完美與平衡，有時候無法容忍他人缺陷。'
+        btn = [
+            MessageTemplateAction(
+                label = '特定場合穿搭顏色建議',
+                text ='1'
+            ),
+            MessageTemplateAction(
+                label = '穿搭顏色心理學',
+                text = '2'
+            ),
+        ]
+        url = 'https://i.imgur.com/COhp0Fl.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
         self.go_back()
 
     def on_exit_wite(self):
@@ -137,10 +212,25 @@ class TocMachine(GraphMachine):
         return text.lower() == "7"
 
     def on_enter_black(self, event):
-        print("I'm entering state1")
-
+        '''print("I'm entering state1")
         reply_token = event.reply_token
         send_text_message(reply_token, "black\n（重新開始）\n'1' 特定場合顏色穿搭'2' 穿搭顏色心理學")
+        self.go_back()'''
+        title = '黑色-穿搭顏色心理學'
+        text = '喜歡黑色的人較不善於表達情感，他們通常壓抑且不太吐露自己的感受，但也渴望被關注、被愛。'
+        btn = [
+            MessageTemplateAction(
+                label = '特定場合穿搭顏色建議',
+                text ='1'
+            ),
+            MessageTemplateAction(
+                label = '穿搭顏色心理學',
+                text = '2'
+            ),
+        ]
+        url = 'https://i.imgur.com/xTuMynx.jpg'
+        send_button_message(event.reply_token, title, text, btn, url)
+
         self.go_back()
 
     def on_exit_black(self):
